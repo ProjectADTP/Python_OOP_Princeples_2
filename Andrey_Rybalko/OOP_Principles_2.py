d36fc328027b251beb448d4af3f76c45200725ef
Andrey <rybalko102@gmail.com>
2026-03-25 11:54:06 +0300

    add Project

---
+++ Andrey_Rybalko/OOP_Principles_2.py
@@ -0,0 +1,29 @@
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+        print("Пополнение успешно! Баланс:", self.__balance)
+
+    def try_withdraw(self, amount):
+        if amount < self.__balance:
+            self.__balance -= amount
+            print("Успешно! Остаток баланса:", self.__balance)
+        else:
+            print("Ошибка! Недостаточно средств для снятия!")
+
+    def check_balance(self):
+        print("Текущий баланс:", self.__balance)
+
+
+def main():
+    bank = BankAccount(100)
+    bank.check_balance()
+    bank.deposit(50)
+    bank.try_withdraw(20)
+    bank.try_withdraw(200)
+    bank.check_balance()
+
+if __name__ == "__main__":
+    main()
